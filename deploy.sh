0b7419d5e5129b3b5cbf126ad15281df4cc1a3e1
Hector Guo <hectorguo@live.com>
2025-02-01 15:32:40 -0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 set -e
+git push origin --delete gh-pages                 # Delete remote branch
+git branch -D gh-pages                           # Delete local branch if it exists
 git checkout --orphan gh-pages
 git --work-tree dist add --all
 git --work-tree dist commit -m 'Deploy'
